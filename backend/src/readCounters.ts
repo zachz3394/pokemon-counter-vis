e2a08fb385e850285e75021a700ab5c09977dc00
Zachary Zhu <zach.z3394@gmail.com>
2022-04-22 15:30:45 -0400

    fix aliasing

--- backend/src/readCounters.ts
+++ backend/src/readCounters.ts
@@ -1,25 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import fs from 'fs';
 import path from 'path';
 import { aliasMap } from './aliases';
 
+const createAliasMap = (jsonData: any) => {
+  const mapAliasToComments = new Map();
+
+  Object.keys(jsonData).sort((a: string, b: string) => {
+    if (b.length > a.length || b > a) {
+      return 1;
+    } else if (b < a) {
+      return -1;
+    }
+    return 0;
+  }).forEach((key: any) => {
+    mapAliasToComments.set(aliasMap.get(key) || key, jsonData[key]['comments']);
+  });
+  return mapAliasToComments;
+}
+
 export const getCounterDataForGen = (genJson: string) => {
   const jsonData = require('../data' + '/' + genJson);
-  const pokemonNames = Object.keys(jsonData).map((key: string) => {
-    return aliasMap.has(key) ? aliasMap.get(key) : key
-  });
+  const aliasMap = createAliasMap(jsonData);
+  const pokemonNames = Array.from(aliasMap.keys());
   const pokenamesRegex = new RegExp(pokemonNames.join('|'), 'g');
   const allCounters: Map<string, string[]> = new Map();
 
-  for (const key in jsonData) {
-    const pokemonName = aliasMap.has(key) ? aliasMap.get(key) : key;
-
-    const comments: string = jsonData[key]['comments'];
+  for (const [name, comments] of aliasMap.entries()) {
     if (comments) {
       const countersString = comments.substring(comments.indexOf('Checks and Counters'));
       const counters = Array.from(countersString.matchAll(pokenamesRegex)).map((match) => match[0]);
       const uniqueCounters = [...new Set(counters)];
-      allCounters.set(pokemonName, uniqueCounters.filter((x) => x !== pokemonName))
+      allCounters.set(name, uniqueCounters.filter((x) => x !== name))
     }
   }
 
